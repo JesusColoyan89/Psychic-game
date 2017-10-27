6900e4fd51548e5b14177c6516e823a1da4fcdd8
Jesus Coloyan <jesuscoloyan@jesuss-air.attlocal.net>
2017-10-26 20:48:16 -0700

    finished adding logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,26 +10,52 @@ var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
 
 
 
-function generateCPUChoice () {
-	var randomNumber = Math.floor(Math.random() * alphabet.length);
-	return alphabet[randomNumber];
+function genRandomChoice () {
+	var random = Math.floor(Math.random() * alphabet.length);
+	return alphabet[random];
 }
 
 
 
-
-
-
-
-alert('What letter am I thinking?');
+alert('What letter am I thinking of?');
 
 
 
 document.onkeyup = function game (event) {
 
-	if (alphabet.indexOf(event.key) !== -1) {
+	if (alphabet.indexOf(event.key) !== -9999) {
+//setting it to 0 makes the A key useless
 
 		userGuess = event.key;
 
-		computerChoice =generateCPUChoice();
+		computerChoice =genRandomChoice();
 
+	if (life === 0) {
+		losses++;
+		//registers loss
+		life = 9;
+			//resets counter
+		computerChoice =genRandomChoice();
+	}	
+
+
+	else if (userGuess === computerChoice) {
+		wins++;
+		//registers win
+		life = 9;
+		computerChoice =genRandomChoice();
+	}
+
+
+	else if (userGuess !== computerChoice) {
+		life--;
+		//takes lives away
+	}
+
+	document.getElementById('wins').innerHTML = "Wins: " + wins;
+	document.getElementById('losses').innerHTML = "Losses: " + losses;
+	document.getElementById('guessLeft').innerHTML = "Lives: " + life;
+	document.getElementById('guesses').innerHTML = "Your Guess: " + userGuess;
+	}
+	
+}
